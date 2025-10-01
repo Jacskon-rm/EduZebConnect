8f5a10650f174799fb71d8be61504fe7043ff287
Jackson Roey <jacksonmahlatse1238@gmail.com>
2025-10-02 00:42:00 +0200

    Update PlaceholderActivities.java

--- app/src/main/java/com/eduzeb/connect/PlaceholderActivities.java
+++ app/src/main/java/com/eduzeb/connect/PlaceholderActivities.java
@@ -1,1 +1,158 @@
 
+package com.eduzeb.connect;
+
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+// Placeholder activities for features coming soon
+
+class MyApplicationsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("My Applications");
+        Toast.makeText(this, "My Applications - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class RecommendationsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Recommendations");
+        Toast.makeText(this, "Recommendations - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class DocumentsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("My Documents");
+        Toast.makeText(this, "Documents - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class NotificationsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Notifications");
+        Toast.makeText(this, "Notifications - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("My Profile");
+        Toast.makeText(this, "Profile - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class NewApplicationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("New Application");
+        Toast.makeText(this, "New Application - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class ForgotPasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Forgot Password");
+        Toast.makeText(this, "Forgot Password - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class SchoolDetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("School Details");
+        Toast.makeText(this, "School Details - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class StudentAnalyticsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Student Analytics");
+        Toast.makeText(this, "Student Analytics - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class ReportsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Reports");
+        Toast.makeText(this, "Reports - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class AdminNotificationsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Admin Notifications");
+        Toast.makeText(this, "Admin Notifications - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class AdminSettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Admin Settings");
+        Toast.makeText(this, "Admin Settings - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Settings");
+        Toast.makeText(this, "Settings - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class DocumentViewerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Document Viewer");
+        Toast.makeText(this, "Document Viewer - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
+
+class ManageSchoolsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Manage Schools");
+        Toast.makeText(this, "Manage Schools - Coming Soon!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
